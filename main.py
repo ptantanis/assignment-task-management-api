dde6371de4619d88022cb4905b544ee12e1f1ffc
Pachara Tantanis <p.tantanis@live.com>
2024-05-08 01:35:15 +0700

    refactor task router into router file in task folder

--- main.py
+++ main.py
@@ -1,65 +1,13 @@
-from uuid import UUID
-from fastapi import Depends, FastAPI, HTTPException, status
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+from task import router
+from task.database import engine
 
-from task import crud, models, schemas
-from task.database import SessionLocal, engine
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app.include_router(router.router, tags=["tasks"])
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.post("/tasks", response_model=schemas.Task, status_code=status.HTTP_201_CREATED)
-def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
-    return crud.create_task(db, task=task)
-
-
-@app.patch("/tasks/{task_id}", response_model=schemas.Task)
-def update_task(
-    task_id: UUID, task_update: schemas.TaskUpdate, db: Session = Depends(get_db)
-):
-    db_task = crud.get_task(db, task_id=task_id)
-    if db_task is None:
-        raise HTTPException(status_code=404, detail="Task not found")
-
-    new_version = schemas.Task.model_validate(db_task).model_copy(
-        update=task_update.model_dump(exclude_unset=True)
-    )
-    new_version.version += 1
-
-    return crud.update_task(db, task=new_version)
-
-
-@app.post("/tasks/{task_id}/undo", response_model=schemas.Task)
-def undo_task(task_id: UUID, db: Session = Depends(get_db)):
-    return crud.undo_task(db, task_id=task_id)
-
-
-@app.get("/tasks/{task_id}", response_model=schemas.Task)
-def get_task(task_id: UUID, db: Session = Depends(get_db)):
-    db_task = crud.get_task(db, task_id=task_id)
-    if db_task is None:
-        raise HTTPException(status_code=404, detail="Task not found")
-    return db_task
-
-
-@app.get("/tasks", response_model=list[schemas.Task])
-def search_tasks(
-    task_filter: schemas.TaskFilter = Depends(), db: Session = Depends(get_db)
-):
-    users = crud.search_tasks(db, task_filter=task_filter)
-    return users
---
+++ task/router.py
@@ -0,0 +1,50 @@
+from uuid import UUID
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from . import crud, models
+from .schemas import Task, TaskCreate, TaskFilter, TaskUpdate
+from .database import get_db, engine
+
+
+models.Base.metadata.create_all(bind=engine)
+
+
+router = APIRouter()
+
+
+@router.post("/tasks", response_model=Task, status_code=status.HTTP_201_CREATED)
+def create_task(task: TaskCreate, db: Session = Depends(get_db)):
+    return crud.create_task(db, task=task)
+
+
+@router.patch("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: UUID, task_update: TaskUpdate, db: Session = Depends(get_db)):
+    db_task = crud.get_task(db, task_id=task_id)
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    new_version = Task.model_validate(db_task).model_copy(
+        update=task_update.model_dump(exclude_unset=True)
+    )
+    new_version.version += 1
+
+    return crud.update_task(db, task=new_version)
+
+
+@router.post("/tasks/{task_id}/undo", response_model=Task)
+def undo_task(task_id: UUID, db: Session = Depends(get_db)):
+    return crud.undo_task(db, task_id=task_id)
+
+
+@router.get("/tasks/{task_id}", response_model=Task)
+def get_task(task_id: UUID, db: Session = Depends(get_db)):
+    db_task = crud.get_task(db, task_id=task_id)
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return db_task
+
+
+@router.get("/tasks", response_model=list[Task])
+def search_tasks(task_filter: TaskFilter = Depends(), db: Session = Depends(get_db)):
+    users = crud.search_tasks(db, task_filter=task_filter)
+    return users
